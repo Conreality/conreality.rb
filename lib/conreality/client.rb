7c01e132383340ec60588e8c9af7e44b390fd2ee
Arto Bendiken <arto@conreality.org>
2017-06-14 23:26:08 +0200

    Implemented Client#send_event.

--- lib/conreality/client.rb
+++ lib/conreality/client.rb
@@ -8,9 +8,29 @@ module Conreality
     attr_accessor :conn
 
     ##
-    # @param [Hash] options
+    # @param  options [Hash]
+    # @option options [String] :dbname
     def initialize(options = {})
       @conn = PG.connect(options)
     end
-  end # module
-end # module
+
+    # @!group Events
+
+    ##
+    # @param  predicate [String] a predicate string
+    # @param  subject   [UUID]   the subject entity
+    # @param  object    [UUID]   the object entity
+    # @return [Integer] the event ID
+    def send_event(predicate, subject, object = nil)
+      predicate = predicate.to_s
+      subject   = subject.to_s
+      object    = object ? object.to_s : nil
+      @conn.exec_params("SELECT public.event_send($1, $2, $3)", [subject, predicate, object]) do |results|
+        result = results.getvalue(0, 0)
+        result ? result.to_i : nil
+      end
+    end
+
+    # @!endgroup
+  end # Client
+end # Conreality
