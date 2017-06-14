3b3871db97ddb24dbd6aff4a8c8b0780ac104d16
Arto Bendiken <arto@conreality.org>
2017-06-14 23:44:09 +0200

    Implemented Client#send_message.

--- lib/conreality/client.rb
+++ lib/conreality/client.rb
@@ -18,14 +18,31 @@ module Conreality
 
     ##
     # @param  predicate [String] a predicate string
-    # @param  subject   [UUID]   the subject entity
-    # @param  object    [UUID]   the object entity
+    # @param  subject   [UUID]   the source object
+    # @param  object    [UUID]   the target object, if any
     # @return [Integer] the event ID
     def send_event(predicate, subject, object = nil)
       predicate = predicate.to_s
       subject   = subject.to_s
       object    = object ? object.to_s : nil
       @conn.exec_params("SELECT public.event_send($1, $2, $3)", [subject, predicate, object]) do |results|
+        result = results.getvalue(0, 0)
+        result ? result.to_i : nil
+      end
+    end
+
+    # @!endgroup
+
+    # @!group Messages
+
+    ##
+    # @param  sender    [UUID]   the sending asset or player
+    # @param  text      [String] the message contents as text
+    # @return [Integer] the message ID
+    # @todo   Support for audio messages.
+    def send_message(sender, text)
+      sender, text = sender.to_s, text.to_s
+      @conn.exec_params("SELECT public.message_send($1, $2)", [sender, text]) do |results|
         result = results.getvalue(0, 0)
         result ? result.to_i : nil
       end
