15090984d95ea0348c55a372c381bf860441e52c
Arto Bendiken <arto@conreality.org>
2017-06-15 00:23:52 +0200

    Implemented Client#call_proc.

--- lib/conreality/client.rb
+++ lib/conreality/client.rb
@@ -26,15 +26,14 @@ module Conreality
       predicate = predicate.to_s
       subject   = subject.to_s
       object    = object ? object.to_s : nil
-      @conn.exec_params("SELECT public.event_send($1, $2, $3)", [subject, predicate, object]) do |results|
-        result = results.getvalue(0, 0)
-        result ? result.to_i : nil
-      end
+
+      result = self.call_proc_with_result('public.event_send($1, $2, $3)', subject, predicate, object)
+      result ? result.to_i : nil
     end
 
     # @!endgroup
 
-    # @!group Messages
+    # @!group Messaging
 
     ##
     # @param  sender    [UUID]   the sending asset or player
@@ -43,10 +42,31 @@ module Conreality
     # @todo   Support for audio messages.
     def send_message(sender, text)
       sender, text = sender.to_s, text.to_s
-      @conn.exec_params("SELECT public.message_send($1, $2)", [sender, text]) do |results|
-        result = results.getvalue(0, 0)
-        result ? result.to_i : nil
+
+      result = self.call_proc_with_result('public.message_send($1, $2)', sender, text)
+      result ? result.to_i : nil
+    end
+
+    # @!endgroup
+
+    # @!group Low-level
+
+    ##
+    # @param  signature [String]
+    # @param  arguments [Array]
+    # @return [Object]
+    def call_proc_with_result(signature, *arguments)
+      self.call_proc(signature, *arguments) do |results|
+        results.getvalue(0, 0)
       end
+    end
+
+    ##
+    # @param  signature [String]
+    # @param  arguments [Array]
+    # @return [PG::Result]
+    def call_proc(signature, *arguments, &block)
+      @conn.exec_params("SELECT #{signature}", arguments, &block)
     end
 
     # @!endgroup
