fe1dbc5cfd8d162d7af5232e1d559b466113dd68
Arto Bendiken <arto@conreality.org>
2017-06-15 00:46:41 +0200

    Implemented Object#as_*().

--- lib/conreality/object.rb
+++ lib/conreality/object.rb
@@ -11,5 +11,29 @@ module Conreality
     def initialize(uuid, client)
       @uuid, @client = uuid.to_s, client
     end
+
+    ##
+    # Returns this object cast to an asset.
+    #
+    # @return [Asset]
+    def as_asset
+      Asset.new(@uuid, @client)
+    end
+
+    ##
+    # Returns this object cast to a camera.
+    #
+    # @return [Camera]
+    def as_camera
+      Camera.new(@uuid, @client)
+    end
+
+    ##
+    # Returns this object cast to a player.
+    #
+    # @return [Player]
+    def as_player
+      Player.new(@uuid, @client)
+    end
   end # Object
 end # Conreality
