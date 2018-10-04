0c2b0dc6a52bcf72683273f78b57fa96d5085737
Arto Bendiken <arto@conreality.org>
2018-10-04 10:37:19 +0300

    Imported generated RPC messages and service stubs.
    Source: https://github.com/conreality/conreality-api

---
+++ lib/conreality/rpc.rb
@@ -0,0 +1,6 @@
+# This is free and unencumbered software released into the public domain.
+
+require 'conreality/rpc/common_pb'
+require 'conreality/rpc/model_pb'
+require 'conreality/rpc/master_pb'
+require 'conreality/rpc/master_services_pb'
